f54190c1a120493ce64718703769e944901c6c7c
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-06-10 12:28:43 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+
+# Remove all stopped containers
+docker container prune -f
+
+# Remove all unused images
+docker image prune -a -f
+
+# Remove all unused volumes
+docker volume prune -f
+
+# Remove all unused networks
+docker network prune -f
+
+echo "Cleanup completed!"
 
