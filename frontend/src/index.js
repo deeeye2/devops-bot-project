9892ad2c4ab48903d3a486766110158b088a0ec9
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-06-13 16:57:45 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';  // Ensure this path is correct
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
